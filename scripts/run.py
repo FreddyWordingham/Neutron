b602d29a7c1cf94cd35d4f2e1cc7a250ab4da06f
FreddyWordingham <freddy@digilab.co.uk>
2023-03-21 17:41:44 +0000

    Wrapped simulator

--- scripts/run.py
+++ scripts/run.py
@@ -1,3 +1,53 @@
 import neutron
 
-print(dir(neutron))
+
+num_threads = 8
+num_steps = 100
+
+colour_map = [
+    "3f007a",
+    "7b0079",
+    "aa0072",
+    "d10065",
+    "f01055",
+    "ff4a42",
+    "ff762b",
+    "ffa004",
+    "ffc900",
+    "fff000",
+]
+
+num_neutrons = 1000000
+block_size = 100
+
+bump_dist = 1e-9
+min_weight = 1e-3
+
+gun_pos = [0.0, 0.0, -1.0]
+gun_target = [0.0, 0.0, 0.0]
+gun_spread = 30.0  # degrees
+
+scat_coeff = 1e-1
+abs_coeff = 1e-1
+mins = [-0.5e-1, -0.5e-1, -0.5e-1]
+maxs = [0.5e-1, 0.5e-1, 0.5e-1]
+
+num_voxels = [128, 128, 128]
+
+neutron.simulate(
+    num_threads,
+    num_steps,
+    colour_map,
+    num_neutrons,
+    block_size,
+    bump_dist,
+    min_weight,
+    gun_pos,
+    gun_target,
+    gun_spread,
+    scat_coeff,
+    abs_coeff,
+    mins,
+    maxs,
+    num_voxels,
+)
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+pub mod colours;
+pub mod data;
+pub mod engine;
+pub mod model;
+pub mod parameters;
+pub mod particle;
+pub mod run;
+
+pub use self::colours::*;
+pub use self::data::*;
+pub use self::engine::*;
+pub use self::model::*;
+pub use self::parameters::*;
+pub use self::particle::*;
+pub use self::run::*;
+
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -6,9 +22,51 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+/// Runs a simulation.
+#[pyfunction]
+fn simulate(
+    num_threads: usize,
+    num_steps: usize,
+    colour_map: Vec<String>,
+    num_neutrons: usize,
+    block_size: usize,
+    bump_dist: f64,
+    min_weight: f64,
+    gun_pos: [f64; 3],
+    gun_target: [f64; 3],
+    gun_spread: f64,
+    scat_coeff: f64,
+    abs_coeff: f64,
+    mins: [f64; 3],
+    maxs: [f64; 3],
+    num_voxels: [usize; 3],
+) -> PyResult<String> {
+    let params = parameters::Parameters {
+        num_threads,
+        num_steps,
+        colour_map,
+        num_neutrons,
+        block_size,
+        bump_dist,
+        min_weight,
+        gun_pos,
+        gun_target,
+        gun_spread,
+        scat_coeff,
+        abs_coeff,
+        mins,
+        maxs,
+        num_voxels,
+    };
+
+    println!("Running!");
+
+    Ok("Simulation complete.".to_string())
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn neutron(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(simulate, m)?)?;
     Ok(())
 }
